3eaed2ff8615d83258294b1db1e10e00949e70f6
venkatesh <venkatesh9ui@gmail.com>
2021-11-12 22:15:38 -0500

    How Component Functions Are Executed -- It won't work this way

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,14 +4,17 @@ import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  let title = props.title;
+
   const clickHandler = () => {
-    console.log('Clicked!!!');
+    title = 'Updated!!!';
+    console.log('Updated!!!');
   };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
