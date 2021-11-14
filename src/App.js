9f04a84025ff35ae237c98804d6edf99ee178347
venkatesh <venkatesh9ui@gmail.com>
2021-11-14 17:45:40 -0500

    Using Stateful Lists

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
 import React from 'react';
+import { useState } from 'react/cjs/react.development';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setExpense] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    // console.log('App.js');
+    // console.log(expense);
+    setExpense((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
 
   // return React.createElement(
   //   'div',
@@ -31,11 +42,6 @@ const App = () => {
   //   React.createElement('h2', {}, "Let's get started"),
   //   React.createElement(Expenses, { items: expenses })
   // );
-
-  const addExpenseHandler = (expense) => {
-    console.log('App.js');
-    console.log(expense);
-  };
 
   return (
     <div>
