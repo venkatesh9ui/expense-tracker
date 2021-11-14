61a99ee1dc7c4cc3626e883469b8e0cf11628b05
venkatesh <venkatesh9ui@gmail.com>
2021-11-14 17:34:40 -0500

    Rendering Lists of Data

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,8 +9,8 @@ const Expenses = (props) => {
   const [filteredyear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = (selectedYear) => {
-    console.log('Expenses.js');
-    // console.log(selectedYear);
+    // console.log('Expenses.js');
+    console.log(selectedYear);
     setFilteredYear(selectedYear);
   };
 
@@ -21,26 +21,13 @@ const Expenses = (props) => {
           selected={filteredyear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
