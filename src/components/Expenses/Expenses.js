120344627a77ab43a7f32ee98fd3ac3c37126238
venkatesh <venkatesh9ui@gmail.com>
2021-11-14 18:57:05 -0500

    Outputting Conditional Content - removed the two types of conditional code

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -38,30 +38,6 @@ const Expenses = (props) => {
           selected={filteredyear}
           onChangeFilter={filterChangeHandler}
         />
-
-        {/* {filteredExpenses.length === 0 ? (
-          <p>No expenses found!</p>
-        ) : (
-          filteredExpenses.map((expense) => (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          ))
-        )} */}
-
-        {/* {filteredExpenses.length === 0 && <p>No expenses found!</p>}
-        {filteredExpenses.length > 0 &&
-          filteredExpenses.map((expense) => (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          ))} */}
         {expensesContent}
       </Card>
     </div>
